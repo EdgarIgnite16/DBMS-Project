9152b03ce2b9ee589be98615a4d33a60bd6fb747
Edgar <edgarignite1704@gmail.com>
2023-02-09 21:14:37 +0700

    update lại bài của Nhi

---
+++ Tuần 1/btap1.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE spud_PTBac01
+@a int, @b int, @c int
+AS
+	BEGIN
+		IF (@a =0 AND @b=0)
+			BEGIN 
+				PRINT 'phuong trinh vô số nghiệm'
+			END 
+		IF (@a !=0 AND @b != 0 )
+			BEGIN
+				PRINT'phuong trinh vo nghiem'
+			END
+		IF (@a!=0 )
+			BEGIN 
+				PRINT'phuong trinh có một nghiệm duy nhất'
+			END
+	END
+GO
+
+EXEC spud_PTBac01 2, 3, 0
